cdc32f536b261a23ef888100e6ab8f43ccbaabde
Peter <peter654qq@gmail.com>
2015-11-26 21:45:30 +0800

    add login/register functions

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,15 @@
 |
 */
 
+// 認證路由...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// 註冊路由...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::get('/', function () {
     return view('welcome');
 });
